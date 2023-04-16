8f2f01689fa83c2fd58f629795cb080886eaf6dd
AlyssaYoung <ypc19@mails.tsinghua.edu.cn>
2023-04-16 22:11:44 +0800

    modify train.py of hcrn_havf

--- HAVF/hcrn_havf/validate.py
+++ HAVF/hcrn_havf/validate.py
@@ -8,7 +8,7 @@ import json
 import pickle
 from termcolor import colored
 
-from DataLoader import VideoQADataLoader
+from DataLoader import AVQADataLoader
 from utils.utils import todevice
 
 import model.HCRN as HCRN
@@ -143,81 +143,19 @@ def validate(cfg, model, data, device, write_preds=False):
     else:
         return acc, all_preds, gts, v_ids, q_ids, which_acc, comefrom_acc, happening_acc, where_acc, why_acc, beforenext_acc, when_acc, usedfor_acc
 
-# def validate(cfg, model, data, device, write_preds=False):
-#     model.eval()
-#     print('validating...')
-#     total_acc, count = 0.0, 0
-#     all_preds = []
-#     gts = []
-#     v_ids = []
-#     q_ids = []
-#     with torch.no_grad():
-#         for batch in tqdm(data, total=len(data)):
-#             video_ids, question_ids, answers, *batch_input = [todevice(x, device) for x in batch]
-#             if cfg.train.batch_size == 1:
-#                 answers = answers.to(device)
-#             else:
-#                 answers = answers.to(device).squeeze()
-#             batch_size = answers.size(0)
-#             logits = model(*batch_input).to(device)
-#             if cfg.dataset.question_type in ['action', 'transition']:
-#                 preds = torch.argmax(logits.view(batch_size, cfg.dataset.ans_count), dim=1)
-#                 agreeings = (preds == answers)
-#             elif cfg.dataset.question_type == 'count':
-#                 answers = answers.unsqueeze(-1)
-#                 preds = (logits + 0.5).long().clamp(min=1, max=10)
-#                 batch_mse = (preds - answers) ** 2
-#             else:
-#                 logits = F.softmax(logits, dim=-1)
-#                 #logits = logits * masks
-#                 preds = logits.detach().argmax(1)
-#                 agreeings = (preds == answers)
-#             if write_preds:
-#                 if cfg.dataset.question_type not in ['action', 'transition', 'count']:
-#                     preds = logits.argmax(1)
-#                 if cfg.dataset.question_type in ['action', 'transition']:
-#                     answer_vocab = data.vocab['question_answer_idx_to_token']
-#                 else:
-#                     answer_vocab = data.vocab['answer_idx_to_token']
-#                 for predict in preds:
-#                     if cfg.dataset.question_type in ['count', 'transition', 'action']:
-#                         all_preds.append(predict.item())
-#                     else:
-#                         all_preds.append(answer_vocab[predict.item()])
-#                 for gt in answers:
-#                     if cfg.dataset.question_type in ['count', 'transition', 'action']:
-#                         gts.append(gt.item())
-#                     else:
-#                         gts.append(answer_vocab[gt.item()])
-#                 for id in video_ids:
-#                     v_ids.append(id.cpu().numpy())
-#                 for ques_id in question_ids:
-#                     q_ids.append(ques_id)
-
-#             if cfg.dataset.question_type == 'count':
-#                 total_acc += batch_mse.float().sum().item()
-#                 count += answers.size(0)
-#             else:
-#                 total_acc += agreeings.float().sum().item()
-#                 count += answers.size(0)
-#         acc = total_acc / count
-#     if not write_preds:
-#         return acc
-#     else:
-#         return acc, all_preds, gts, v_ids, q_ids
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', dest='cfg_file', help='optional config file', default='tgif_qa_action.yml', type=str)
     parser.add_argument('--app_feat', default='resnet101', type=str)
-    parser.add_argument('--motion_feat', default='resnet101', type=str)
+    parser.add_argument('--motion_feat', default='resnext101', type=str)
+    parser.add_argument('--audio_feat', default='PANNs', type=str)
     args = parser.parse_args()
     if args.cfg_file is not None:
         cfg_from_file(args.cfg_file)
 
-    assert cfg.dataset.name in ['tgif-qa', 'msrvtt-qa', 'msvd-qa']
-    assert cfg.dataset.question_type in ['frameqa', 'count', 'transition', 'action', 'none']
+    # assert cfg.dataset.name in ['tgif-qa', 'msrvtt-qa', 'msvd-qa']
+    # assert cfg.dataset.question_type in ['frameqa', 'count', 'transition', 'action', 'none']
     # check if the data folder exists
     assert os.path.exists(cfg.dataset.data_dir)
 
@@ -229,39 +167,21 @@ if __name__ == '__main__':
     loaded = torch.load(ckpt, map_location='cpu')
     model_kwargs = loaded['model_kwargs']
 
-    if cfg.dataset.name == 'tgif-qa':
-        cfg.dataset.test_question_pt = os.path.join(cfg.dataset.data_dir,
-                                                    cfg.dataset.test_question_pt.format(cfg.dataset.name, cfg.dataset.question_type))
-        cfg.dataset.vocab_json = os.path.join(cfg.dataset.data_dir, cfg.dataset.vocab_json.format(cfg.dataset.name, cfg.dataset.question_type))
+    # cfg.dataset.question_type = 'none'
+    cfg.dataset.appearance_feat = '{}_appearance_{}_feat.h5'
+    cfg.dataset.motion_feat = '{}_motion_{}_feat.h5'
+    cfg.dataset.vl_audio_feat = '{}_vlaudio_{}_feat.h5'
+    cfg.dataset.vocab_json = '{}_vocab.json'
+    cfg.dataset.test_question_pt = '{}_val_questions.pt'
 
-        cfg.dataset.appearance_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.appearance_feat.format(cfg.dataset.name, cfg.dataset.question_type))
-        cfg.dataset.motion_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.motion_feat.format(cfg.dataset.name, cfg.dataset.question_type))
-    elif cfg.dataset.name == 'msvd-qa':
-        cfg.dataset.question_type = 'none'
-        cfg.dataset.appearance_feat = '{}_appearance_{}_feat.h5'
-        cfg.dataset.motion_feat = '{}_motion_{}_feat.h5'
-        cfg.dataset.vocab_json = '{}_vocab.json'
-        cfg.dataset.test_question_pt = '{}_test_RoBERTa_questions.pt'
+    cfg.dataset.test_question_pt = os.path.join(cfg.dataset.data_dir,
+                                                cfg.dataset.test_question_pt.format(cfg.dataset.name))
+    cfg.dataset.vocab_json = os.path.join(cfg.dataset.data_dir, cfg.dataset.vocab_json.format(cfg.dataset.name))
 
-        cfg.dataset.test_question_pt = os.path.join(cfg.dataset.data_dir,
-                                                    cfg.dataset.test_question_pt.format(cfg.dataset.name))
-        cfg.dataset.vocab_json = os.path.join(cfg.dataset.data_dir, cfg.dataset.vocab_json.format(cfg.dataset.name))
+    cfg.dataset.appearance_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.appearance_feat.format(cfg.dataset.name, args.app_feat))
+    cfg.dataset.motion_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.motion_feat.format(cfg.dataset.name, args.motion_feat))
+    cfg.dataset.vl_audio_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.vl_audio_feat.format(cfg.dataset.name, args.audio_feat))
 
-        cfg.dataset.appearance_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.appearance_feat.format(cfg.dataset.name, args.app_feat))
-        cfg.dataset.motion_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.motion_feat.format(cfg.dataset.name, args.motion_feat))
-    else:
-        cfg.dataset.question_type = 'none'
-        cfg.dataset.appearance_feat = '{}_appearance_{}_feat.h5'
-        cfg.dataset.motion_feat = '{}_motion_{}_feat.h5'
-        cfg.dataset.vocab_json = '{}_vocab.json'
-        cfg.dataset.test_question_pt = '{}_test_questions.pt'
-
-        cfg.dataset.test_question_pt = os.path.join(cfg.dataset.data_dir,
-                                                    cfg.dataset.test_question_pt.format(cfg.dataset.name))
-        cfg.dataset.vocab_json = os.path.join(cfg.dataset.data_dir, cfg.dataset.vocab_json.format(cfg.dataset.name))
-
-        cfg.dataset.appearance_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.appearance_feat.format(cfg.dataset.name, args.app_feat))
-        cfg.dataset.motion_feat = os.path.join(cfg.dataset.data_dir, cfg.dataset.motion_feat.format(cfg.dataset.name, args.motion_feat))
 
     test_loader_kwargs = {
         'question_type': cfg.dataset.question_type,
@@ -269,15 +189,14 @@ if __name__ == '__main__':
         'vocab_json': cfg.dataset.vocab_json,
         'appearance_feat': cfg.dataset.appearance_feat,
         'motion_feat': cfg.dataset.motion_feat,
+        'vl_audio_feat': cfg.dataset.vl_audio_feat,
         'test_num': cfg.test.test_num,
         'batch_size': cfg.train.batch_size,
         'num_workers': cfg.num_workers,
-        'shuffle': False,
-        'sent_dim':cfg.train.sent_dim,
-        'augment': False
+        'shuffle': False
     }
-    test_loader = VideoQADataLoader(**test_loader_kwargs)
-    model_kwargs.update({'vocab': test_loader.vocab, 'ans_count':cfg.dataset.ans_count})
+    test_loader = AVQADataLoader(**test_loader_kwargs)
+    model_kwargs.update({'vocab': test_loader.vocab})
     model = HCRN.HCRNNetwork(**model_kwargs).to(device)
     model.load_state_dict(loaded['state_dict'])
 
@@ -309,28 +228,29 @@ if __name__ == '__main__':
                 ans_candidates = obj['ans_candidates']
 
             for idx in range(len(org_q_ids)):
-                dict[str(org_q_ids[idx])] = [org_v_names[idx], questions[idx], ans_candidates[idx]]
-            instances = [
-                {'video_id': video_id, 'question_id': q_id, 'video_name': dict[str(q_id)][0], 'question': [vocab[word.item()] for word in dict[str(q_id)][1] if word != 0],
-                 'answer': answer,
-                 'prediction': pred} for video_id, q_id, answer, pred in
-                zip(np.hstack(v_ids).tolist(), np.hstack(q_ids).tolist(), gts, preds)]
+                dict[str(org_q_ids[idx])] = [org_v_names[idx], org_v_ids[idx], org_q_ids[idx], questions[idx], ans_candidates[idx]]
+
+            instances = []
+            for video_id, q_id, answer, pred in zip(np.hstack(v_ids).tolist(), np.hstack(q_ids).tolist(), gts, preds):
+                if answer != pred:
+                    instances.append({'question_id': dict[str(q_id)][2], 'video_name': dict[str(q_id)][0],
+                                    'answer': answer, 'prediction': pred})
             # write preditions to json file
             with open(preds_file, 'w') as f:
                 json.dump(instances, f)
             sys.stdout.write('Display 10 samples...\n')
             # Display 10 samples
-            for idx in range(10):
-                print('Video name: {}'.format(dict[str(q_ids[idx].item())][0]))
-                cur_question = [vocab[word.item()] for word in dict[str(q_ids[idx].item())][1] if word != 0]
-                print('Question: ' + ' '.join(cur_question) + '?')
-                all_answer_cands = dict[str(q_ids[idx].item())][2]
-                for cand_id in range(len(all_answer_cands)):
-                    cur_answer_cands = [vocab[word.item()] for word in all_answer_cands[cand_id] if word
-                                        != 0]
-                    print('({}): '.format(cand_id) + ' '.join(cur_answer_cands))
-                print('Prediction: {}'.format(preds[idx]))
-                print('Groundtruth: {}'.format(gts[idx]))
+            # for idx in range(10):
+            #     print('Video name: {}'.format(dict[str(q_ids[idx].item())][0]))
+            #     cur_question = [vocab[word.item()] for word in dict[str(q_ids[idx].item())][1] if word != 0]
+            #     print('Question: ' + ' '.join(cur_question) + '?')
+            #     all_answer_cands = dict[str(q_ids[idx].item())][2]
+            #     for cand_id in range(len(all_answer_cands)):
+            #         cur_answer_cands = [vocab[word.item()] for word in all_answer_cands[cand_id] if word
+            #                             != 0]
+            #         print('({}): '.format(cand_id) + ' '.join(cur_answer_cands))
+            #     print('Prediction: {}'.format(preds[idx]))
+            #     print('Groundtruth: {}'.format(gts[idx]))
         else:
             vocab = test_loader.vocab['question_idx_to_token']
             dict = {}
