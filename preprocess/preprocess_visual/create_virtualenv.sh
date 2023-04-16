1817fa7a5192ce33c216161bb55911fb736cc588
AlyssaYoung <ypc19@mails.tsinghua.edu.cn>
2023-04-17 01:48:49 +0800

    update .gitignore

--- preprocess/preprocess_visual/create_virtualenv.sh
+++
@@ -1,5 +0,0 @@
-conda create -n preprocess_visual python=3.6
-conda activate preprocess_visual
-conda install -c conda-forge ffmpeg
-conda install -c conda-forge scikit-video
-pip install -r requirements.txt
--- preprocess/preprocess_visual/extract_appearance_feat.sh
+++
@@ -1,9 +0,0 @@
-python preprocess_visual.py \
-    --gpu_id 0 \
-    --dataset avqa \
-    --model resnet101 \
-    --feature_type appearance \
-    --video_path data/video \
-    --video_name_mapping data/video/video_map.json \
-    --annotation_path data/annotation \
-    --out_path data/feats/avqa_appearance_resnet101_feat.h5
